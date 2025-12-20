83f6f39f08346458b44365554a77e0b1426abe1a
Mayur Prajapati <mproyur685@gmail.com>
2025-12-20 22:38:46 +0530

    chore(db): setup Prisma ORM with MySQL

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -5,6 +5,7 @@ import v2Router from './routers/v2/index.router';
 import { appErrorHandler, genericErrorHandler } from './middlewares/error.middleware';
 import logger from './config/logger.config';
 import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
+import { prisma } from "./lib/prisma";
 const app = express();
 
 app.use(express.json());
@@ -26,9 +27,17 @@ app.use(appErrorHandler);
 app.use(genericErrorHandler);
 
 
-app.listen(serverConfig.PORT, () => {
+const server = app.listen(serverConfig.PORT, () => {
     logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
     logger.info(`Press Ctrl+C to stop the server.`);
 
     console.log(`Server is running on http://localhost:${serverConfig.PORT}`)
+});
+
+process.on("SIGINT", async () => {
+  console.log("Shutting down server...");
+  await prisma.$disconnect();
+  server.close(() => {
+    process.exit(0);
+  });
 });
