d46214573a01b7aedac1271032dfe6133a7f0d4b
Mayur Prajapati <mproyur685@gmail.com>
2025-12-25 15:15:33 +0530

    chore(db): add drizzle migrations for schema changes

---
+++ Backend/drizzle/0006_marvelous_white_queen.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `slot_templates` MODIFY COLUMN `price_1_day` int;--> statement-breakpoint
+ALTER TABLE `slot_templates` MODIFY COLUMN `price_3_day` int;--> statement-breakpoint
+ALTER TABLE `slot_templates` MODIFY COLUMN `price_7_day` int;
